3d5938b637982cd79352ae9ae501b982a070bff5
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-04-09 19:54:42 +0000

    🛠 FIX: Changed local base url to env variable

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,9 @@
 import Navigation from "../components/navigation";
 import '../assets/css/dashboard.css'
-import { Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL } from '../components/urls'
 
-const baseURL = "http://localhost:8080/api/v1";
 const Dashboard = () => {
     const [totalInvestments, setTotalInvestments] = useState(null)
     const [totalClients, setTotalClients] = useState(null)
@@ -13,8 +12,8 @@ const Dashboard = () => {
     // make multiple api calls to backend
     const getBackendData = () => {
         let endpoints = [
-            `${baseURL}/clients/total`,
-            `${baseURL}/investments/total`
+            `${BASE_URL}/clients/total`,
+            `${BASE_URL}/investments/total`
         ];
         // if one of our promises fails, the entire request fail
         Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(([{ data: clients }, { data: investments }]) => {
@@ -42,27 +41,27 @@ const Dashboard = () => {
                     </div>}
 
                 <section className="flex-container">
-                    {(totalClients>=0) && (totalInvestments>=0) && (
+                    {(totalClients >= 0) && (totalInvestments >= 0) && (
                         <>
                             <div className="cards investments-card">
-                            <div className="card-body">
-                                <p className=" text-muted">total investments</p>
-                                <h1>{totalInvestments}</h1>
+                                <div className="card-body">
+                                    <p className=" text-muted">total investments</p>
+                                    <h1>{totalInvestments}</h1>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="cards client-card">
-                            <div className="card-body">
-                                <p className=" text-muted">total clients</p>
-                                <h1>{totalClients}</h1>
+                            <div className="cards client-card">
+                                <div className="card-body">
+                                    <p className=" text-muted">total clients</p>
+                                    <h1>{totalClients}</h1>
+                                </div>
                             </div>
-                        </div>
 
                         </>
                     )}
                 </section>
 
-               
+
             </div>
         </div>
     );
