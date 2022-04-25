8de4ad12e37a245e6d5b2b018260e09091d05dd9
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-04-25 17:21:37 +0000

    FEAT: Implement lopreader spinner

--- src/components/preLoader.js
+++ src/components/preLoader.js
@@ -1,5 +1,12 @@
+import '../assets/css/loader.css'
+
 const Preloader = () => {
-  return (<div>Loading</div>);
+  return (<>
+    <div className='loader-glistening-window'></div>
+    <p>Loading</p>
+  </>
+
+  );
 }
 
 export default Preloader;
